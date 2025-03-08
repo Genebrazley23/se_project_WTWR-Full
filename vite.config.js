e135569412d4dde313aa5b91c4e8baaa4ec5ea13
Genebrazley23 <166544100+Genebrazley23@users.noreply.github.com>
2025-03-07 19:27:02 -0600

    change

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/se_project_WTWR-Full/",
   plugins: [react()],
   server: {
     port: 3001,
+  },
+  build: {
+    sourcemap: true,
+    polyfillDynamicImport: true,
+  },
+  resolve: {
+    mainFields: ["module", "main"],
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: /node_modules\/.*\.jsx?$/,
   },
 });
