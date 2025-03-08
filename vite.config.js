243843ecd1ceeab0a9187894ec9880a1383686e4
Genebrazley23 <166544100+Genebrazley23@users.noreply.github.com>
2025-03-07 20:10:21 -0600

    push

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/se_project_WTWR-Full/",
   plugins: [react()],
@@ -5,15 +9,13 @@ export default defineConfig({
     port: 3001,
   },
   build: {
-    sourcemap: true,
-    polyfillDynamicImport: true,
-  },
-  resolve: {
-    mainFields: ["module", "main"],
-  },
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    exclude: /node_modules\/.*\.jsx?$/,
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
   },
 });
